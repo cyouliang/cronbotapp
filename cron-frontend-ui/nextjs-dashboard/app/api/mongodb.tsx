6942535170cf32919ea42943fc6d34df18a1ef86
youliang <yoyoliang97@gmail.com>
2024-04-15 22:31:36 +0800

    comment error codes temporarily

--- cron-frontend-ui/nextjs-dashboard/app/api/mongodb.tsx
+++ cron-frontend-ui/nextjs-dashboard/app/api/mongodb.tsx
@@ -15,16 +15,16 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Add Mongo URI to .env.local')
 }
 
-if (process.env.NODE_ENV === 'development') {
-Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-}
+// if (process.env.NODE_ENV === 'development') {
+// Replacement).
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options)
+//     global._mongoClientPromise = client.connect()
+//   }
+//   clientPromise = global._mongoClientPromise
+// } else {
+//   client = new MongoClient(uri, options)
+//   clientPromise = client.connect()
+// }
 
 export default clientPromise
