8148df84232c401e330fafb2694ef3e5896fb2e1
youliang <yoyoliang97@gmail.com>
2024-05-23 12:03:38 +0800

    fix auth/login page

--- cron-frontend-ui/nextjs-dashboard/app/api/auth/login/page.tsx
+++ cron-frontend-ui/nextjs-dashboard/app/api/auth/login/page.tsx
@@ -5,16 +5,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const { email, password } = req.body
-    await signIn('credentials', { email, password })
+  // try {
+  //   const { email, password } = req.body
+  //   await signIn('credentials', { email, password })
  
-    res.status(200).json({ success: true })
-  } catch (error) {
-    if (error instanceof Error && error.name === 'CredentialsSignin') {
-      res.status(401).json({ error: 'Invalid credentials.' })
-    } else {
-      res.status(500).json({ error: 'Something went wrong.' })
-    }
-  }
+  //   res.status(200).json({ success: true })
+  // } catch (error) {
+  //   if (error instanceof Error && error.name === 'CredentialsSignin') {
+  //     res.status(401).json({ error: 'Invalid credentials.' })
+  //   } else {
+  //     res.status(500).json({ error: 'Something went wrong.' })
+  //   }
+  // }
 }
