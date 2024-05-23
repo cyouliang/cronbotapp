9d4fe3a1a4ce85cfc968adae0bfc22b97df3099c
youliang <yoyoliang97@gmail.com>
2024-05-23 11:51:17 +0800

    comment out auth-login file

--- cron-frontend-ui/nextjs-dashboard/app/api/auth/login/page.tsx
+++ cron-frontend-ui/nextjs-dashboard/app/api/auth/login/page.tsx
@@ -1,20 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { signIn } from 'next-auth/react'
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import { signIn } from 'next-auth/react'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const { email, password } = req.body
-    await signIn('credentials', { email, password })
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const { email, password } = req.body
+//     await signIn('credentials', { email, password })
  
-    res.status(200).json({ success: true })
-  } catch (error) {
-    if (error instanceof Error && error.name === 'CredentialsSignin') {
-      res.status(401).json({ error: 'Invalid credentials.' })
-    } else {
-      res.status(500).json({ error: 'Something went wrong.' })
-    }
-  }
-}
+//     res.status(200).json({ success: true })
+//   } catch (error) {
+//     if (error instanceof Error && error.name === 'CredentialsSignin') {
+//       res.status(401).json({ error: 'Invalid credentials.' })
+//     } else {
+//       res.status(500).json({ error: 'Something went wrong.' })
+//     }
+//   }
+// }
