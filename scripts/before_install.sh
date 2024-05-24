b0999bee6968b058a02af64f12953d63f2f0b1bb
youliang <yoyoliang97@gmail.com>
2024-05-24 13:21:42 +0800

    changed apt-get to yum

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'run before_install.sh: ' >>/cron-telebot2/deploy.log
 
-echo 'sudo apt-get install python-pip' >> /cron-telebot2/deploy.log 
-sudo apt-get install python-pip
+echo 'sudo yum install python-pip' >> /cron-telebot2/deploy.log 
+sudo yum install python-pip
