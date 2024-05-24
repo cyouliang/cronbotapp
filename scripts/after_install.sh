b11fb083d271afa7008724885277b7c34cdd009e
youliang <yoyoliang97@gmail.com>
2024-05-24 12:24:12 +0800

    edited appspec yml

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/cronbot_app/deploy.log
+echo 'run after_install.sh: ' >>/cron-telebot2/deploy.log
 
-echo 'cd /cron-telebot 2' >> /home/ec2-user/cronbot_app/deploy.log
-cd 'cron-telebot 2' >> /home/ec2-user/cronbot_app/deploy.log
+echo 'cd /cron-telebot 2' >> /cron-telebot2/deploy.log
+cd 'cron-telebot 2' >> /cron-telebot2/deploy.log
 
-echo 'pip install -r requirements.txt' >> /home/ec2-user/cronbot_app/deploy.log 
-pip install -r requirements.txt >> /home/ec2-user/cronbot_app/deploy.log
+echo 'pip install -r requirements.txt' >> /cron-telebot2/deploy.log 
+pip install -r requirements.txt >> /cron-telebot2/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/cronbot_app/deploy.log
+echo 'run application_start.sh: ' >> /cron-telebot2/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'gunicorn main:app -k uvicorn.workers.UvicornWorker --timeout 60' >> /home/ec2-user/cronbot_app/deploy.log
-gunicorn main:app -k uvicorn.workers.UvicornWorker --timeout 60 >> /home/ec2-user/cronbot_app/deploy.log
+echo 'gunicorn main:app -k uvicorn.workers.UvicornWorker --timeout 60' >> /cron-telebot2/deploy.log
+gunicorn main:app -k uvicorn.workers.UvicornWorker --timeout 60 >> /cron-telebot2/deploy.log
